13d051b57aab8cb897b6339be1506d05e0c35a6c
muhsinmuhsy <muhsinkoodali856@gmail.com>
2024-01-10 12:24:48 +0530

    none

--- assets/mine/js/main.js
+++ assets/mine/js/main.js
@@ -1,37 +1,25 @@
-// function fadeInUp() {
-//     var fadeInUps = document.querySelectorAll(".fadeInUp",".backInLeft");
-  
-//     for (var i = 0; i < fadeInUps.length; i++) {
-//       var windowHeight = window.innerHeight;
-//       var elementTop = fadeInUps[i].getBoundingClientRect().top;
-//       var elementVisible = 150;
-  
-//       if (elementTop < windowHeight - elementVisible) {
-//         fadeInUps[i].classList.add("active");
-//       } else {
-//         fadeInUps[i].classList.remove("active");
-//       }
-//     }
-//   }
-  
-// window.addEventListener("scroll", fadeInUp);
-function ScrollAnimation(event) {
-    event.preventDefault(); // Prevent default scroll behavior
-  
-    var elements = document.querySelectorAll(".backInLeft, .backInRight, .fadeInUp, .flipInX, .shakeX, .zoomIn");
-  
-    for (var i = 0; i < elements.length; i++) {
-      var windowHeight = window.innerHeight;
-      var elementTop = elements[i].getBoundingClientRect().top;
-      var elementVisible = 150;
-  
-      if (elementTop < windowHeight - elementVisible) {
-        elements[i].classList.add("active");
+
+
+
+function handleIntersection(entries, observer) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('active');
       } else {
-        elements[i].classList.remove("active");
+        entry.target.classList.remove('active');
       }
-    }
+    });
   }
-  
-  window.addEventListener("scroll", ScrollAnimation);
+
+  const observer = new IntersectionObserver(handleIntersection, { threshold: 0.5 });
+
+  document.querySelectorAll('.fadeInUp').forEach(element => {
+    observer.observe(element);
+  });
+
+
+
+    
+
+
   
