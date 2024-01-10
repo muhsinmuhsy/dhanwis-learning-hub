4954a173e703d08752d93ae4c6bc52a32ecec525
muhsinmuhsy <muhsinkoodali856@gmail.com>
2024-01-10 10:55:23 +0530

    first commit

---
+++ assets/mine/js/main.js
@@ -0,0 +1,35 @@
+// function fadeInUp() {
+//     var fadeInUps = document.querySelectorAll(".fadeInUp",".backInLeft");
+  
+//     for (var i = 0; i < fadeInUps.length; i++) {
+//       var windowHeight = window.innerHeight;
+//       var elementTop = fadeInUps[i].getBoundingClientRect().top;
+//       var elementVisible = 150;
+  
+//       if (elementTop < windowHeight - elementVisible) {
+//         fadeInUps[i].classList.add("active");
+//       } else {
+//         fadeInUps[i].classList.remove("active");
+//       }
+//     }
+//   }
+  
+// window.addEventListener("scroll", fadeInUp);
+
+function ScrollAnimation() {
+    var elements = document.querySelectorAll(".backInLeft, .backInRight, .fadeInUp, .flipInX, .shakeX, .zoomIn");
+
+    for (var i = 0; i < elements.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = elements[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+
+      if (elementTop < windowHeight - elementVisible) {
+        elements[i].classList.add("active");
+      } else {
+        elements[i].classList.remove("active");
+      }
+    }
+  }
+
+  window.addEventListener("scroll", ScrollAnimation);
